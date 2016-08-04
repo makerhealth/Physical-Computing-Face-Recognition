9ef6f3cc0144bae84ad11c1bc7c5a5304643bcf5
mp.schrader <maxphilipp.schrader@hotmail.de>
2016-08-03 21:49:44 -0400

    Added Storage Functionality

--- livedetection.py
+++ livedetection.py
@@ -4,6 +4,7 @@ from picamera import PiCamera
 import imutils
 import cv2
 import time
+import datetime
 
 #  DEFINE CONSTANTS FOR CAMERA ON RASPBERRY PI
 framerate = 16
@@ -19,6 +20,8 @@ time.sleep(camera_warmup_time)
 
 #  Instantiate FaceDetector object and pass it HarrCascade XML face classifier
 fd = FaceDetector("haarcascade_frontalface_default.xml")
+file_name = "./faces_"+str(time.time())+".csv"
+counter = 0
 
 #  Begin Capturing frames from camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
@@ -28,10 +31,21 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     faceRects = fd.detect(gray, scaleFactor = 1.1, minNeighbors = 5,
                           minSize = (30, 30))
     frameClone = image.copy()
+
     for(fX, fY, fW, fH) in faceRects:
         cv2.rectangle(frameClone, (fX, fY), (fX + fW, fY + fH),
                      (0, 255, 0), 2)
-        
+
+    current_time = time.localtime()
+    number_of_faces = len(faceRects)
+    print time.strftime('%Y-%m-%d_%H:%M:%S', current_time) + ";" + str(number_of_faces)
+    if number_of_faces > 0:
+        try:
+            f = open(file_name, "a")
+            f.write(time.strftime('%Y-%m-%d_%H:%M:%S', current_time) + ";" + str(number_of_faces)+"\n")
+            f.close()
+        except:
+            print "Error saving datapoit"
     cv2.imshow("Face", frameClone)
     if cv2.waitKey(1) & 0xFF == ord("q"):
            break
